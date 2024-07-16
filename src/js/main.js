62a05b733208458d9b3b3ab3e46bdd02aad1d950
Yessenia C <colocho.yessenia@gmail.com>
2024-07-16 11:11:41 +0200

    api Animes

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,29 @@
-'use strict';
+"use strict";
+
+const inputSearch = document.querySelector(".search");
+const buttonSearch = document.querySelector(".button-search");
+const animeApi = document.querySelector('.animes__search')
+
+let arreyAnimes =[]
+
+function handleInput(event) {
+  event.preventDefault();
+  const infoSearch = inputSearch.value;
+
+  fetch(`https://api.jikan.moe/v4/anime?q=${infoSearch}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const infoAnimes = data.data;
+
+    for(let anime of infoAnimes){
+        const img = anime.images.jpg.image_url
+        const name = anime.titles[0].title
+    }
 
 
+    });
+
+
+}
+
+buttonSearch.addEventListener("click", handleInput);
