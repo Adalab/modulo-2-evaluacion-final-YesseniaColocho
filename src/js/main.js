e2f3dd0287446c48fdac2e55a51b0dbec54861c5
Yessenia C <colocho.yessenia@gmail.com>
2024-07-16 19:59:46 +0200

    js

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,28 @@
 
 const inputSearch = document.querySelector(".search");
 const buttonSearch = document.querySelector(".button-search");
-const animeApi = document.querySelector('.animes__search')
+const animeContainer = document.querySelector(".animes__search");
+const animeFavorites = document.querySelector(".anime__favorites");
 
-let arreyAnimes =[]
+let resultAnimes = [];
+let favoriteAnimes = [];
+
+
+function handleAnimeClick(e) {
+  const animeId = e.currentTarget.id;
+  const foundFavoriteAnime = favoriteAnimes.find((anime)=> parseInt(animeId)=== anime.mal_id)
+  if(!foundFavoriteAnime){
+  const animeResult = resultAnimes.find((anime) => parseInt(animeId) === anime.mal_id);
+  favoriteAnimes.push(animeResult);
+  animeFavorites.innerHTML= ""
+
+  for(let favoriteAnime of favoriteAnimes){
+    let img = favoriteAnime.images.jpg.image_url
+    let name = favoriteAnime.title
+    animeFavorites.innerHTML += `<div><img src="${img}" /> ${name}</div>`
+  }
+}
+}
 
 function handleInput(event) {
   event.preventDefault();
@@ -13,17 +32,26 @@ function handleInput(event) {
   fetch(`https://api.jikan.moe/v4/anime?q=${infoSearch}`)
     .then((response) => response.json())
     .then((data) => {
+      animeContainer.innerHTML = "";
       const infoAnimes = data.data;
+      resultAnimes = data.data;
 
-    for(let anime of infoAnimes){
-        const img = anime.images.jpg.image_url
-        const name = anime.titles[0].title
-    }
+      for (let anime of infoAnimes) {
+        let img = anime.images.jpg.image_url;
 
+        if (!img) {
+          img = `https://via.placeholder.com/210x295/fafafa/666666/?text=Animu-TV`;
+        }
 
+        const name = anime.title;
+        animeContainer.innerHTML += `<div class="anime" id="${anime.mal_id}" ><img src="${img}"/> ${name}</div>`;
+      }
+
+      let animeElements = document.querySelectorAll(".anime");
+      for (let animeElement of animeElements) {
+        animeElement.addEventListener("click", handleAnimeClick);
+      }
     });
-
-
 }
 
 buttonSearch.addEventListener("click", handleInput);
